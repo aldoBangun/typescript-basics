cd0acaef02ac96a0c387ff1868682c9276dfc5d9
aldoBangun <punk.aldo68@gmail.com>
2022-09-24 12:06:14 +0800

    Add functions types

---
+++ src/functions.ts
@@ -0,0 +1,32 @@
+// return values & argument types
+
+function add(a: number, b: number): number {
+  return a + b
+}
+
+const total1: number = add(2, 2)
+console.log(total1)
+
+function printName(name: string): void {
+  const message = `Your names is ${name}`
+  console.log(message)
+}
+
+printName('Aldo')
+
+// function as a type
+type Multiply = (a: number, b: number) => number
+type Relationship = (name1: string, name2: string) => void
+
+const multiply: Multiply = (a, b) => {
+  return a * b
+}
+
+multiply(20, 10)
+
+const loveCalculator: Relationship = (a, b) => {
+  const score: number = Math.round(Math.random() * 100)
+  console.log(`${a} + ${b} = ${score} / 100`)
+}
+
+loveCalculator('Aldo', 'Jenny Blackpink')
