23e4b2c0c928a011afdc6c5b12380c5c2e550c7f
aldoBangun <punk.aldo68@gmail.com>
2022-09-24 10:41:59 +0800

    add basictypes

---
+++ src/basicTypes.ts
@@ -0,0 +1,21 @@
+// String
+const fruit: string = 'Papaya'
+const occupation: string = 'Front End Web Developer'
+
+// Number
+const age: number = 24
+const price: number = 299
+
+// Boolean
+const isOnline: boolean = true
+const hasSubmitted: boolean = false
+
+// Any
+let random: any = 'test'
+random = 10
+
+// Array
+const numbers: number[] = [1, 2, 3]
+const fruits: Array<String> = ['Apple', 'Strawberry', 'Banana']
+const anything: any[] = [1, '2', 3, 'four', 'five', 6, true]
+
