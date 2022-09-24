0e0164df0dae9616ba26f73e52c202d1e9766fac
aldoBangun <punk.aldo68@gmail.com>
2022-09-24 19:25:16 +0800

    Add class

---
+++ src/class.ts
@@ -0,0 +1,51 @@
+class Car {
+  public name: string
+  public weight: number
+
+  constructor(name: string, weight: number) {
+    this.name = name
+    this.weight = weight
+  }
+
+  public drive(): void {
+    console.log('driving...')
+  }
+
+  public brake(): void {
+    console.log('perform brake')
+  }
+}
+
+// inheritance
+class SportCar extends Car {
+  public turbo(): void {
+    console.log('perform nitros')
+  }
+}
+
+class Truck extends Car {
+  public size: number
+  private _oil: number
+
+  constructor(name: string, weight: number, size: number) {
+    super(name, weight)
+    this.size = size
+    this._oil = size * 4
+  }
+
+  public loadStuff(): void {
+    console.log('load some goods')
+  }
+
+  set oil(value: number) {
+    this._oil = this.size * value
+  }
+
+  get oil(): number {
+    return this._oil 
+  }
+}
+
+const myTruck = new Truck('test', 20, 20)
+myTruck.oil = 99
+console.log(myTruck.oil)
