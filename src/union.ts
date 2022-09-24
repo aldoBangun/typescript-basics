bc1732e81b7cd4e900d90b6e0978232e3b61efd1
aldoBangun <punk.aldo68@gmail.com>
2022-09-24 12:12:06 +0800

    add union type

---
+++ src/union.ts
@@ -0,0 +1,10 @@
+type Phone = string | number
+type Loading = string | boolean
+
+const myPhone: Phone = 628594939234
+const yourPhone: Phone = '0843949343'
+const herPhone: string | number = 43848999292
+const hisPhone: string | number = '08348439933'
+
+const loadingProduct: Loading = 'idle'
+const loadingUser: Loading = true
